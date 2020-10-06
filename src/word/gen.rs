0157e5951a639e562a01ce793865ec3919e371b7
Ferenc Fabian <qwer.kocka@gmail.com>
2020-10-06 10:01:29 +0200

    Iterator trait implementation for word gen (#13)
    * Add combination meg permutation
    
    * Start Heap algorithm
    
    * Working on Heap algorithm
    
    * Finish Heap algorithm
    
    * Add Variation
    
    * Add map_state
    
    * Add map_state
    
    * Add working example for map_state
    
    * Add working next function
    
    * Add iterator

--- src/word/gen.rs
+++ src/word/gen.rs
@@ -3,37 +3,23 @@ use std::cell::RefCell;
 pub struct Variations {
     state: RefCell<Vec<usize>>,
     char_set: Vec<String>,
-    m: usize, // length of the generated
     n: usize, // length of the char_set
 
-    position: usize, // state of the
+    position: usize, // position counter of the state
 }
 
 impl Variations {
     pub fn new(char_set: Vec<String>, m: usize) -> Self {
-        //let s = vec![0, m];
         let mut s = Vec::new();
         s.resize_with(m, || 0);
         Variations {
-            m: m,
             n: char_set.len(),
             state: RefCell::new(s),
             char_set: char_set,
             position: 0,
         }
     }
-
-    pub fn next(&mut self) -> Option<String> {
-        if self.state.borrow_mut()[self.position] >= self.n-1 {
-            self.state.borrow_mut()[self.position] = 0;
-            self.position += 1;
-        }
-
-        self.state.borrow_mut()[self.position]+=1;
-
-        self.map_state()
-    }
-
+ 
     fn map_state(&self) -> Option<String> {
         let mut result = Vec::<String>::new();
         for v in self.state.borrow_mut().clone().into_iter() {
@@ -41,6 +27,21 @@ impl Variations {
         }
 
         Some(result.join(""))
+    }
+}
+
+impl Iterator for Variations {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        if self.state.borrow_mut()[self.position] >= self.n - 1 {
+            self.state.borrow_mut()[self.position] = 0;
+            self.position += 1;
+        }
+
+        self.state.borrow_mut()[self.position] += 1;
+
+        self.map_state()
     }
 }
 
@@ -75,14 +76,11 @@ mod tests {
 
         let mut v = Variations::new(available_chars, 3);
         for _ in 1..10 {
-        match v.next() {
-            Some(s) => println!("{}", s),
-            None => {}
-        };
+
+            match v.next() {
+                Some(s) => println!("{}", s),
+                None => {}
+            };
         }
     }
-    // fn test_permutations() {
-    // let available_chars = vec!("a", "b", "c", "d", "e");
-    // let h = Handler::new(available_chars);
-    // }
 }

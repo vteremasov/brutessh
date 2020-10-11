1b7907092f50eef2549a76d7f34e5004e89e4266
Ferenc Fabian <qwer.kocka@gmail.com>
2020-10-11 22:10:07 +0200

    Add new algorithm to iterate over on the possible passwords (#18)
    * Add test case for new permutation algorithm
    
    * Start the new algorithm
    
    * Finish new variation algorithms

--- src/word/gen.rs
+++ src/word/gen.rs
@@ -6,7 +6,7 @@ pub struct Variations {
     n: usize, // length of the char_set
     m: usize, // capacity of the state
 
-    position: usize, // position counter of the state
+    initial: bool,
 }
 
 impl Variations {
@@ -18,10 +18,10 @@ impl Variations {
             m: m,
             state: RefCell::new(s),
             char_set: char_set,
-            position: 0,
+            initial: true,
         }
     }
- 
+
     fn map_state(&self) -> Option<String> {
         let mut result = Vec::<String>::new();
         for v in self.state.borrow_mut().clone().into_iter() {
@@ -35,19 +35,38 @@ impl Variations {
 impl Iterator for Variations {
     type Item = String;
 
+    // next using a specific algorithm
+    // 1. increment the 0 position
+    // 2. if the 0 pos is greater or equal with the char len then
+    // 3a. increment the next position, and if it's greater, then
+    // 4a. increment the next position and continue this
+    // 3b. if the next position is the end of the state then return None
     fn next(&mut self) -> Option<String> {
-        if self.state.borrow_mut()[self.position] >= self.n - 1 {
-            self.state.borrow_mut()[self.position] = 0;
-            self.position += 1;
+        let pos = 0;
+        if self.initial {
+            self.initial = false;
+            return self.map_state();
         }
 
-        if self.position >= self.m {
-            return None;
-        }
+        self.state.borrow_mut()[pos] += 1;
 
-        println!("n: {}, m: {}, position: {}, index: {}", self.n, self.m, self.position, self.state.borrow()[self.position]);
-        
-        self.state.borrow_mut()[self.position] += 1;
+        if self.state.borrow()[pos] >= self.n {
+            let mut counter = 1;
+            loop {
+                if pos + counter >= self.m {
+                    return None;
+                }
+                self.state.borrow_mut()[pos] = 0;
+                self.state.borrow_mut()[pos + counter] += 1;
+                if self.state.borrow()[pos + counter] >= self.n {
+                    self.state.borrow_mut()[pos + counter] = 0;
+                    counter += 1;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
 
         self.map_state()
     }
@@ -84,11 +103,44 @@ mod tests {
 
         let mut v = Variations::new(available_chars, 3);
         for _ in 1..10 {
-
             match v.next() {
                 Some(s) => println!("{}", s),
                 None => {}
             };
+        }
+    }
+
+    #[test]
+    fn test_permutation() {
+        let results = [
+            "aaa", "baa", "caa", "aba", "bba", "cba", "aca", "bca", "cca", "aab", "bab", "cab",
+            "abb", "bbb", "cbb", "acb", "bcb", "ccb", "aac", "bac", "cac", "abc", "bbc", "cbc",
+            "acc", "bcc", "ccc",
+        ];
+
+        let split = "abc".split("");
+        let mut available_chars: Vec<String> =
+            split.map(|s| String::from(s)).collect::<Vec<String>>();
+        available_chars.drain(0..1);
+        available_chars.drain(available_chars.len() - 1..);
+        println!("{:?}", available_chars);
+
+        let mut v = Variations::new(available_chars, 3);
+        let mut counter = 0;
+        loop {
+            match v.next() {
+                Some(s) => {
+                    if s != String::from(results[counter]) {
+                        panic!(format!(
+                            "Counter: {}, Result: {}, Variation: {}",
+                            counter, results[counter], s
+                        ));
+                    }
+                }
+                None => break,
+            };
+
+            counter += 1;
         }
     }
 }
